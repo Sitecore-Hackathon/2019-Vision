c2c201824f3f69591510801371520621c2e86255
dgharbieh <dgharbieh@tanasuk.com>
2019-03-02 20:12:09 +0200

    fix bug of item select by default

--- src/Feature/DynamicItemsSelection/Hackathon.Feature.DynamicItemsSelection/Commands/ToggleItemSelector.cs
+++ src/Feature/DynamicItemsSelection/Hackathon.Feature.DynamicItemsSelection/Commands/ToggleItemSelector.cs
@@ -3,35 +3,33 @@ using Sitecore.Shell;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Web;
 using System;
-using System.Web;
-using System.Web.UI;
-
 namespace Hackathon.Feature.DynamicItemsSelection.Commands
 {
-  
+
     [Serializable]
     public class ToggleItemSelector : Command
     {
         public override void Execute(CommandContext context)
         {
-           
+
         }
 
+        //The following overriden function is called when the "Item Selector" checkbox is checked or unchecked
         public override string GetClick(CommandContext context, string click)
         {
-          
-            var script = "javascript:ItemSelector_Click();";                                                        
-
-            return script;
+            if (string.IsNullOrEmpty(WebUtil.GetCookieValue("scItemCheckboxState")))
+            {
+                WebUtil.SetCookieValue("scItemCheckboxState", "0");
+            }
+            return "javascript:ItemSelector_Click();";
         }
-
-        
 
         public override CommandState QueryState(CommandContext context)
         {
             return CheckboxState ? CommandState.Down : CommandState.Enabled;
         }
 
+        //The property gets the cookie value for the "Item Selector" checkbox in the ribbon, and sets the value to 1 if the cookie value is there..otherwise returns 0
         private static bool CheckboxState
         {
             get

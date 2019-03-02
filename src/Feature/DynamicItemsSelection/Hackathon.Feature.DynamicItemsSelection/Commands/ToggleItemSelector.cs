c0d13566ca7adbec6b4c2bad493e94d0f04e0bf5
jelaydi <jelaydi@gmail.com>
2019-03-02 13:34:45 +0200

    Added ToggleItemSelector Class

---
+++ src/Feature/DynamicItemsSelection/Hackathon.Feature.DynamicItemsSelection/Commands/ToggleItemSelector.cs
@@ -0,0 +1,30 @@
+﻿
+using Sitecore.Shell;
+using Sitecore.Shell.Framework.Commands;
+using System;
+
+namespace Hackathon.Feature.DynamicItemsSelection.Commands
+{
+  
+    [Serializable]
+    public class ToggleItemSelector : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+        }
+
+        public override string GetClick(CommandContext context, string click)
+        {
+            return "ItemSelector_Click";
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            //if (!UserOptions.ContentEditor.ShowRawValues)
+           // {
+                return CommandState.Enabled;
+           // }
+           // return CommandState.Down;
+        }
+    }
+}

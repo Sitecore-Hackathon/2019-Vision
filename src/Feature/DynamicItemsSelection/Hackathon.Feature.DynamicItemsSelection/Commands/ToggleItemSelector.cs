0a4e8cfdd6d30c68949235930f824501499de184
jelaydi <jelaydi@gmail.com>
2019-03-02 15:31:52 +0200

    Added Function to catch the check click of the ribbon checkbox

--- src/Feature/DynamicItemsSelection/Hackathon.Feature.DynamicItemsSelection/Commands/ToggleItemSelector.cs
+++ src/Feature/DynamicItemsSelection/Hackathon.Feature.DynamicItemsSelection/Commands/ToggleItemSelector.cs
@@ -1,7 +1,10 @@
 ﻿
 using Sitecore.Shell;
 using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web;
 using System;
+using System.Web;
+using System.Web.UI;
 
 namespace Hackathon.Feature.DynamicItemsSelection.Commands
 {
@@ -11,20 +14,34 @@ namespace Hackathon.Feature.DynamicItemsSelection.Commands
     {
         public override void Execute(CommandContext context)
         {
+           
         }
 
         public override string GetClick(CommandContext context, string click)
         {
-            return "ItemSelector_Click";
+          
+            var script = "javascript:ItemSelector_Click();";                                                        
+
+            return script;
         }
+
+        
 
         public override CommandState QueryState(CommandContext context)
         {
-            //if (!UserOptions.ContentEditor.ShowRawValues)
-           // {
-                return CommandState.Enabled;
-           // }
-           // return CommandState.Down;
+            return CheckboxState ? CommandState.Down : CommandState.Enabled;
+        }
+
+        private static bool CheckboxState
+        {
+            get
+            {
+                return WebUtil.GetCookieValue("scItemCheckboxState") != "0";
+            }
+            set
+            {
+                WebUtil.SetCookieValue("scItemCheckboxState", value ? "1" : "0");
+            }
         }
     }
 }
